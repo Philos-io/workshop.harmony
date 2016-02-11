a14e628e2bf91d4319604f4d4ba6e757714c100a
davy engone <davy@philos.io>
2016-02-10 17:07:29 -0800

    removed workshops folder

--- workshops/02_testing_refactoring/components/navcontroller.js
+++
@@ -1,9 +0,0 @@
-(function(){
-  'use strict';
-
-    function NavController() {
-      this.title = "Book Store";
-    }
-
-    angular.module('bookstore').controller('NavController',NavController);
-})();
